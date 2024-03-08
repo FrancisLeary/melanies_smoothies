4bcebcb59e1c728c4f9b846106c709d2bc5e8a7a
Lucas Morillo-Mendez <48600035+FrancisLeary@users.noreply.github.com>
2024-03-08 14:31:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,13 @@ if ingredients_list:
 	
             st.success('Your Smoothie is ordered', icon="✅")
 
+# New section to display fruityvice nutrition info
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
+
+
 
 
 
